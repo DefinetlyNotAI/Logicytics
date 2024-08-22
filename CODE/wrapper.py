f1476f2d1426fc9800d76b3ae6f1f82125e67bf4
DefinatlyNotAI <Nirt_12023@outlook.com>
2024-08-22 14:16:15 +0300

    Made codbase tidier, added config.json -> Issue #31 and #35

--- CODE/wrapper.py
+++ CODE/wrapper.py
@@ -1,222 +1,9 @@
 import ctypes
-import os
-from datetime import datetime
-import colorlog
 import subprocess
 import argparse
+import json
+from Custom_Libraries.Log import *
 
-# if __name__ == "__main__":
-#     action, powerdown = Actions().flags()
-
-
-# os.makedirs("../ACCESS/LOGS/", exist_ok=True)
-# log = Log(filename="../ACCESS/LOGS/Logicytics.log", debug=True)
-
-
-class Log:
-    def __init__(
-            self,
-            filename="Server.log",
-            err_filename=None,
-            use_colorlog=True,
-            debug=False,
-            debug_color="cyan",
-            info_color="green",
-            warning_color="yellow",
-            error_color="red",
-            critical_color="red",
-            colorlog_fmt_parameters="%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
-    ):
-        """
-        Initializes a new instance of the LOG class.
-
-        The log class logs every interaction when called in both colorlog and in the log file
-
-        Best to only modify filename, and DEBUG.
-
-        Only if you are planning to use the dual-log parameter that allows you to both log unto the shell and the log file:
-            IMPORTANT: This class requires colorlog to be installed and also uses it in the INFO level,
-            To use the debug level, set DEBUG to True.
-
-            If you are using colorlog, DO NOT INITIALIZE IT MANUALLY, USE THE LOG CLASS PARAMETER'S INSTEAD.
-            Sorry for any inconvenience that may arise.
-
-        Args:
-            filename (str, optional): The name of the log file. Defaults to "Server.log".
-            use_colorlog (bool, optional): Whether to use colorlog. Defaults to True.
-            debug (bool, optional): Whether to use the debug level. Defaults to False (which uses the INFO level).
-            debug_color (str, optional): The color of the debug level. Defaults to "cyan".
-            info_color (str, optional): The color of the info level. Defaults to "green".
-            warning_color (str, optional): The color of the warning level. Defaults to "yellow".
-            error_color (str, optional): The color of the error level. Defaults to "red".
-            critical_color (str, optional): The color of the critical level. Defaults to "red".
-            colorlog_fmt_parameters (str, optional): The format of the log message. Defaults to "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s".
-
-        Returns:
-            None
-        """
-        self.color = use_colorlog
-        if self.color:
-            # Configure colorlog for logging messages with colors
-            logger = colorlog.getLogger()
-            if debug:
-                logger.setLevel(
-                    colorlog.DEBUG
-                )  # Set the log level to DEBUG to capture all relevant logs
-            else:
-                logger.setLevel(
-                    colorlog.INFO
-                )  # Set the log level to INFO to capture all relevant logs
-            handler = colorlog.StreamHandler()
-            formatter = colorlog.ColoredFormatter(
-                colorlog_fmt_parameters,
-                datefmt=None,
-                reset=True,
-                log_colors={
-                    "DEBUG": debug_color,
-                    "INFO": info_color,
-                    "WARNING": warning_color,
-                    "ERROR": error_color,
-                    "CRITICAL": critical_color,
-                },
-            )
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
-
-
-        self.filename = str(filename)
-        if err_filename is None:
-            self.err_filename = self.filename
-        else:
-            self.err_filename = str(err_filename)
-        if not os.path.exists(self.filename):
-            self.__only("|" + "-" * 19 + "|" + "-" * 13 + "|" + "-" * 154 + "|")
-            self.__only(
-                "|     Timestamp     |  LOG Level  |"
-                + " " * 71
-                + "LOG Messages"
-                + " " * 71
-                + "|"
-            )
-        self.__only("|" + "-" * 19 + "|" + "-" * 13 + "|" + "-" * 154 + "|")
-
-    @staticmethod
-    def __timestamp() -> str:
-        """
-        Returns the current timestamp as a string in the format 'YYYY-MM-DD HH:MM:SS'.
-
-        Returns:
-            str: The current timestamp.
-        """
-        now = datetime.now()
-        time = f"{now.strftime('%Y-%m-%d %H:%M:%S')}"
-        return time
-
-    def __only(self, message):
-        """
-        Logs a quick message to the log file.
-
-        Args:
-            message: The message to be logged.
-
-        Returns:
-            None
-        """
-        with open(self.filename, "a") as f:
-            f.write(f"{str(message)}\n")
-
-    @staticmethod
-    def __pad_message(message):
-        """
-        Adds spaces to the end of a message until its length is exactly 153 characters.
-
-        Parameters:
-        - message (str): The input message string.
-
-        Returns:
-        - str: The padded message with a length of exactly 153 characters.
-        """
-        # Calculate the number of spaces needed
-        num_spaces = 153 - len(message)
-
-        if num_spaces > 0:
-            # If the message is shorter than 153 characters, add spaces to the end
-            padded_message = message + " " * num_spaces
-        else:
-            # If the message is already longer than 153 characters, truncate it to the first 153 characters
-            padded_message = message[:150]
-            padded_message += "..."
-
-        padded_message += "|"
-        return padded_message
-
-    def info(self, message):
-        """
-        Logs an informational message to the log file.
-
-        Args:
-            message: The message to be logged.
-
-        Returns:
-            None
-        """
-        if self.color:
-            colorlog.info(message)
-        with open(self.filename, "a") as f:
-            f.write(
-                f"[{self.__timestamp()}] > INFO:     | {self.__pad_message(str(message))}\n"
-            )
-
-    def warning(self, message):
-        """
-        Logs a warning message to the log file.
-
-        Args:
-            message: The warning message to be logged.
-
-        Returns:
-            None
-        """
-        if self.color:
-            colorlog.warning(message)
-        with open(self.filename, "a") as f:
-            f.write(
-                f"[{self.__timestamp()}] > WARNING:  | {self.__pad_message(str(message))}\n"
-            )
-
-    def error(self, message):
-        """
-        Logs an error message to the log file.
-
-        Args:
-            message: The error message to be logged.
-
-        Returns:
-            None
-        """
-        if self.color:
-            colorlog.error(message)
-        with open(self.err_filename, "a") as f:
-            f.write(
-                f"[{self.__timestamp()}] > ERROR:    | {self.__pad_message(str(message))}\n"
-            )
-
-    def critical(self, message):
-        """
-        Writes a critical message to the log file.
-
-        Args:
-            message: The critical message to be logged.
-
-        Returns:
-            None
-        """
-        if self.color:
-            colorlog.critical(message)
-        with open(self.err_filename, "a") as f:
-            f.write(
-                f"[{self.__timestamp()}] > CRITICAL: | {self.__pad_message(str(message))}\n"
-            )
 
 class Checks:
     def __init__(self):
@@ -256,7 +43,7 @@ class Actions:
         parser.add_argument("--silent", action="store_true")
         parser.add_argument("--reboot", action="store_true")
         parser.add_argument("--shutdown", action="store_true")
-        parser.add_argument("--debug", action="store_true")
+        parser.add_argument("--DEBUG", action="store_true")
         parser.add_argument("--modded", action="store_true")
         parser.add_argument("--speedy", action="store_true")
         parser.add_argument("--basic", action="store_true")
@@ -312,3 +99,30 @@ class Actions:
         else:
             print("Error: Only one flag is allowed with the --reboot and --shutdown and --webhook flags.")
             exit(1)
+
+    @staticmethod
+    def read_config():
+        try:
+            with open('../SYSTEM/config.json', 'r') as file:
+                data = json.load(file)
+
+                webhook_url = data.get('WEBHOOK_URL', '')
+                debug = data.get('DEBUG', False)
+                version = data.get('VERSION', '2.0.0')
+                files = data.get('FILES', [])
+
+                if not (all(isinstance(file, str) for file in files) and isinstance(webhook_url, str) and isinstance(debug, bool) and isinstance(version, str)):
+                    print("Invalid config.json format.")
+                    exit(1)
+
+                return webhook_url, debug, version, files
+        except FileNotFoundError:
+            print("config.json not found.")
+            exit(1)
+
+
+if __name__ == "__main__":
+    WEBHOOK, DEBUG, VERSION, FILES = Actions.read_config()
+    os.makedirs("../ACCESS/LOGS/", exist_ok=True)
+    log = Log(filename="../ACCESS/LOGS/Logicytics.log", debug=DEBUG)
+    action, sub_action = Actions().flags()
