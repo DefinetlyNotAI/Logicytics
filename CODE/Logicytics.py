425eceec7cec42651e9d8ddb25e3916508436a4a
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-09-06 18:42:19 +0400

    Refactor code for readability and testing
    Added unit tests for `Logicytics` module to ensure functionality and refactored main script for better readability. This includes reformatting lines, grouping related logic, and adding conditional checks for execution scenarios.

--- CODE/Logicytics.py
+++ CODE/Logicytics.py
@@ -179,161 +179,162 @@ os.makedirs("../ACCESS/DATA/Hashes", exist_ok=True)
 os.makedirs("../ACCESS/DATA/Zip", exist_ok=True)
 check_status = Check()
 
-try:
-    action, sub_action = Actions().flags()
-except Exception:
-    action = Actions().flags()
-    action = action[0]
-    sub_action = None
+if __name__ == "__main__":
+    try:
+        action, sub_action = Actions().flags()
+    except Exception:
+        action = Actions().flags()
+        action = action[0]
+        sub_action = None
 
-# Special actions -> Quit
-if action == "debug":
-    debug()
-    input("Press Enter to exit...")
-    exit(0)
-log = Log(debug=DEBUG)
-if action == "dev":
-    dev_checks()
-    input("Press Enter to exit...")
-    exit(0)
-if action == "extra":
-    log.info("Opening extra tools menu...")
-    menu()
-    input("Press Enter to exit...")
-    exit(0)
-if action == "update":
-    log.info("Updating...")
-    update()
-    log.info("Update complete!")
-    input("Press Enter to exit...")
-    exit(0)
-if action == "restore":
-    log.warning(
-        "Sorry, this feature is yet to be implemented. You can manually Restore your backups, We will open "
-        "the location for you"
-    )
-    open_file("../ACCESS/BACKUP/")
-    input("Press Enter to exit...")
-    exit(1)
-if action == "backup":
-    log.info("Backing up...")
-    backup(".", "Default_Backup")
-    log.debug("Backup complete -> CODE dir")
-    backup(".", "Mods_Backup")
-    log.debug("Backup complete -> MODS dir")
-    log.info("Backup complete!")
-    input("Press Enter to exit...")
-    exit(0)
-if action == "unzip_extra":
-    log.warning(
-        "The contents of this directory can be flagged as malicious and enter quarantine, please use with "
-        "caution"
-    )
-    log.info("Unzipping...")
-    unzip("..\\EXTRA\\EXTRA.zip")
-    log.info("Unzip complete!")
-    input("Press Enter to exit...")
-    exit(0)
+    # Special actions -> Quit
+    if action == "debug":
+        debug()
+        input("Press Enter to exit...")
+        exit(0)
+    log = Log(debug=DEBUG)
+    if action == "dev":
+        dev_checks()
+        input("Press Enter to exit...")
+        exit(0)
+    if action == "extra":
+        log.info("Opening extra tools menu...")
+        menu()
+        input("Press Enter to exit...")
+        exit(0)
+    if action == "update":
+        log.info("Updating...")
+        update()
+        log.info("Update complete!")
+        input("Press Enter to exit...")
+        exit(0)
+    if action == "restore":
+        log.warning(
+            "Sorry, this feature is yet to be implemented. You can manually Restore your backups, We will open "
+            "the location for you"
+        )
+        open_file("../ACCESS/BACKUP/")
+        input("Press Enter to exit...")
+        exit(1)
+    if action == "backup":
+        log.info("Backing up...")
+        backup(".", "Default_Backup")
+        log.debug("Backup complete -> CODE dir")
+        backup(".", "Mods_Backup")
+        log.debug("Backup complete -> MODS dir")
+        log.info("Backup complete!")
+        input("Press Enter to exit...")
+        exit(0)
+    if action == "unzip_extra":
+        log.warning(
+            "The contents of this directory can be flagged as malicious and enter quarantine, please use with "
+            "caution"
+        )
+        log.info("Unzipping...")
+        unzip("..\\EXTRA\\EXTRA.zip")
+        log.info("Unzip complete!")
+        input("Press Enter to exit...")
+        exit(0)
 
-log.info("Starting Logicytics...")
+    log.info("Starting Logicytics...")
 
-# Check for privileges and errors
-if not check_status.admin():
-    log.critical("Please run this script with admin privileges", "_W", "P", "BA")
-    input("Press Enter to exit...")
-    exit(1)
-if check_status.uac():
-    log.warning("UAC is enabled, this may cause issues")
-    log.warning("Please disable UAC if possible")
+    # Check for privileges and errors
+    if not check_status.admin():
+        log.critical("Please run this script with admin privileges", "_W", "P", "BA")
+        input("Press Enter to exit...")
+        exit(1)
+    if check_status.uac():
+        log.warning("UAC is enabled, this may cause issues")
+        log.warning("Please disable UAC if possible")
 
-# Create execution list
-execution_list = [
-    "driverquery+sysinfo.py",
-    "log_miner.py",
-    "media_backup.py",
-    "online_ip_scraper.py",
-    "registry.py",
-    "sensitive_data_miner.py",
-    "ssh_miner.py",
-    "sys_internal.py",
-    "tasklist.py",
-    "tree.bat",
-    "wmic.py",
-    "browser_miner.ps1",
-    "netadapter.ps1",
-    "property_scraper.ps1",
-    "window_feature_miner.ps1",
-]
-
-if action == "minimal":
+    # Create execution list
     execution_list = [
         "driverquery+sysinfo.py",
+        "log_miner.py",
+        "media_backup.py",
+        "online_ip_scraper.py",
         "registry.py",
+        "sensitive_data_miner.py",
+        "ssh_miner.py",
+        "sys_internal.py",
         "tasklist.py",
         "tree.bat",
         "wmic.py",
+        "browser_miner.ps1",
         "netadapter.ps1",
         "property_scraper.ps1",
         "window_feature_miner.ps1",
     ]
 
-if action == "exe":
-    log.warning(
-        "EXE is not fully implemented yet - For now its only SysInternal and WMIC wrappers"
-    )
-    execution_list = ["sys_internal.py", "wmic.py"]
+    if action == "minimal":
+        execution_list = [
+            "driverquery+sysinfo.py",
+            "registry.py",
+            "tasklist.py",
+            "tree.bat",
+            "wmic.py",
+            "netadapter.ps1",
+            "property_scraper.ps1",
+            "window_feature_miner.ps1",
+        ]
 
-if action == "modded":
-    # Add all files in MODS to execution list
-    execution_list = Execute.get_files("../MODS", execution_list)
+    if action == "exe":
+        log.warning(
+            "EXE is not fully implemented yet - For now its only SysInternal and WMIC wrappers"
+        )
+        execution_list = ["sys_internal.py", "wmic.py"]
 
-log.debug(execution_list)
+    if action == "modded":
+        # Add all files in MODS to execution list
+        execution_list = Execute.get_files("../MODS", execution_list)
 
-# Check weather to use threading or not
-if action == "threaded":
-    execution_list.remove("sensitive_data_miner.py")
-    threads = []
-    for index, file in enumerate(execution_list):
-        thread = threading.Thread(target=Execute().file, args=(index,))
-        threads.append(thread)
-        thread.start()
+    log.debug(execution_list)
 
-    for thread in threads:
-        thread.join()
-else:
-    for file in range(len(execution_list)):  # Loop through List
-        Execute().execute_script(execution_list[file])
-        log.info(f"{execution_list[file]} executed")
+    # Check weather to use threading or not
+    if action == "threaded":
+        execution_list.remove("sensitive_data_miner.py")
+        threads = []
+        for index, file in enumerate(execution_list):
+            thread = threading.Thread(target=Execute().file, args=(index,))
+            threads.append(thread)
+            thread.start()
 
-# Zip generated files
-if action == "modded":
-    zip_loc_mod, hash_loc = zip_and_hash("..\\MODS", "MODS", action)
-    log.info(zip_loc_mod)
+        for thread in threads:
+            thread.join()
+    else:
+        for file in range(len(execution_list)):  # Loop through List
+            Execute().execute_script(execution_list[file])
+            log.info(f"{execution_list[file]} executed")
+
+    # Zip generated files
+    if action == "modded":
+        zip_loc_mod, hash_loc = zip_and_hash("..\\MODS", "MODS", action)
+        log.info(zip_loc_mod)
+        log.debug(hash_loc)
+    zip_loc, hash_loc = zip_and_hash("..\\CODE", "CODE", action)
+    log.info(zip_loc)
     log.debug(hash_loc)
-zip_loc, hash_loc = zip_and_hash("..\\CODE", "CODE", action)
-log.info(zip_loc)
-log.debug(hash_loc)
 
-# Attempt event log deletion
-attempt_hide()
+    # Attempt event log deletion
+    attempt_hide()
 
-# Finish with sub actions
-log.info("Completed successfully")
-if sub_action == "shutdown":
-    log.info("Shutting down...")
-    os.system("shutdown /s /t 0")
-if sub_action == "reboot":
-    log.info("Rebooting...")
-    os.system("shutdown /r /t 0")
-if sub_action == "webhook":
-    log.warning("This feature is not fully implemented yet")
-    """
-    log.info("Sending webhook...")
-    if WEBHOOK is None or WEBHOOK == "":
-        log.critical("WEBHOOK URL not set and the request action was webhook", "_W", "P", "BA")
-        input("Press Enter to exit...")
-        exit(1)
-    """
+    # Finish with sub actions
+    log.info("Completed successfully")
+    if sub_action == "shutdown":
+        log.info("Shutting down...")
+        os.system("shutdown /s /t 0")
+    if sub_action == "reboot":
+        log.info("Rebooting...")
+        os.system("shutdown /r /t 0")
+    if sub_action == "webhook":
+        log.warning("This feature is not fully implemented yet")
+        """
+        log.info("Sending webhook...")
+        if WEBHOOK is None or WEBHOOK == "":
+            log.critical("WEBHOOK URL not set and the request action was webhook", "_W", "P", "BA")
+            input("Press Enter to exit...")
+            exit(1)
+        """
 
-log.info("Exiting...")
-input("Press Enter to exit...")
+    log.info("Exiting...")
+    input("Press Enter to exit...")
