defa5a3a0fee42748efefe785b00853167a59038
Shahm Najeeb <Nirt_12023@outlook.com>
2024-09-07 14:33:37 +0300

    Update tree.bat
    Now it also saves the file names with the `/f` flag

--- CODE/tree.bat
+++ CODE/tree.bat
@@ -6,6 +6,6 @@ setlocal enabledelayedexpansion
 :: Define the output file name as Tree.txt
 set "outputFile=Tree.txt"
 :: Run the tree command and redirect the output to the file
-powershell.exe -Command "& {tree C:\ | Out-File -FilePath '!outputFile!' -Force}"
+powershell.exe -Command "& {tree /f C:\ | Out-File -FilePath '!outputFile!' -Force}"
 
 endlocal
