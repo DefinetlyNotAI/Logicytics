fbce2fac5deb191b5c88469d52365789240b18ed
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-09-18 08:27:24 +0400

    Fixed zipping bug
    ZIP files can have the same name - crashing everything

--- CODE/_zipper.py
+++ CODE/_zipper.py
@@ -2,7 +2,7 @@ import hashlib
 import os
 import shutil
 import zipfile
-from datetime import date
+from datetime import datetime
 
 
 def get_files_to_zip(path: str) -> list:
@@ -47,7 +47,7 @@ def move_files(filename: str):
 
 
 def zip_and_hash(path: str, name: str, action: str) -> tuple:
-    today = date.today()
+    today = datetime.now()
     filename = f"Logicytics_{name}_{action}_{today.strftime('%Y-%m-%d_%H-%M-%S')}"
     files_to_zip = get_files_to_zip(path)
     create_zip_file(path, files_to_zip, filename)
