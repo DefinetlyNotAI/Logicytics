939045910061bd7570dd86d32c8dec7530e02dd8
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-10-02 15:54:36 +0400

    Formatted code
    Also tested and refactored roadmap.html, fixed bug with the logger system (None values and settings)

--- CODE/__lib_class.py
+++ CODE/__lib_class.py
@@ -490,4 +490,6 @@ class Execute:
 
 DEBUG, VERSION, API_KEY, CURRENT_FILES = Actions.read_config()
 if __name__ == "__main__":
-    Log().exception("This is a library file and should not be executed directly.", Exception)
+    Log().exception(
+        "This is a library file and should not be executed directly.", Exception
+    )
