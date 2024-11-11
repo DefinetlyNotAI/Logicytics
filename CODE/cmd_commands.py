3cfeb342a31477179bc251b7c32ed166d937675a
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-11-11 19:57:04 +0400

    Fixed some bugs
    Introduced dir_list.py to fix issue with cmd_commands.py 4th command line, now its fixed but still clunky

--- CODE/cmd_commands.py
+++ CODE/cmd_commands.py
@@ -4,28 +4,28 @@ if __name__ == "__main__":
     log = Log({"log_level": DEBUG})
 
 
-def command(file: str, com: str, message: str):
+def command(file: str, commands: str, message: str, encoding: str = "UTF-8") -> None:
     """
     Executes a command and writes the output to a file.
 
     Args:
         file (str): The name of the file to write the command output to.
-        com (str): The command to be executed.
+        commands (str): The command to be executed.
         message (str): A message to be logged.
+        encoding (str): The encoding to write the file in.
 
     Returns:
         None
     """
+    log.info(f"Executing {message}")
     try:
-        output = Actions.run_command(com)
-        open(file, "w").write(output)
-        log.info(f"{message} Successful")
+        output = Actions.run_command(commands)
+        open(file, "w", encoding=encoding).write(output)
+        log.info(f"{message} Successful - {file}")
     except Exception as e:
         log.error(f"Error while getting {message}: {e}")
-    log.info(f"{message} Executed")
 
 
 command("Drivers.txt", "driverquery /v", "Driver Query")
 command("SysInfo.txt", "systeminfo", "System Info")
-command("GPResult.txt", "GPResult /r", "GPResult")
-command("Dir_Root.txt", "Get-ChildItem C:\\ -Recurse", "Root Directory Listing")
+command("GPResult.txt", "GPResult /r", "GPResult", "windows-1252")
