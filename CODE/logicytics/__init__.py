381f4a7d8593672228117ab1b400cdd83f01699d
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-11-30 16:37:05 +0400

    Added deprecation module,
    Also, now will start developing in 3.13

--- CODE/logicytics/__init__.py
+++ CODE/logicytics/__init__.py
@@ -4,6 +4,18 @@ from logicytics.Logger import *
 from logicytics.Checks import *
 from logicytics.FileManagement import *
 from logicytics.Flag import *
+import functools
+
+
+def deprecated(removal_version: str, reason: str) -> callable:
+    def decorator(func: callable) -> callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> callable:
+            print(f"\033[91mDeprecationWarning: A call to the deprecated function {func.__name__}() has been called, {reason}. Function will be removed at version {removal_version}\033[0m")
+            return func(*args, **kwargs)
+        return wrapper
+
+    return decorator
 
 
 Execute = Execute()
@@ -11,6 +23,5 @@ Get = Get()
 Check = Check()
 FileManagement = FileManagement()
 Flag = Flag()
-
 
 DEBUG, VERSION, CURRENT_FILES, DELETE_LOGS = Get.config_data()
