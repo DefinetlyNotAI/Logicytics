5272abf40a15b1dd0310b8c7acc7dadba98f748c
DefinetlyNotAI <Shahm_Najeeb@outlook.com>
2026-08-16 02:56:49 +0300

    chore: reset implementation for v4 architecture

--- CODE/netadapter.ps1
+++
@@ -1,3 +0,0 @@
-# Get all network details
-Write-Output "INFO: Getting NetAdapter Info"
-Get-NetAdapter | Select-Object Name, Status, MacAddress, ifIndex, InterfaceAlias, InterfaceDescription | Out-File -FilePath .\Network.txt
