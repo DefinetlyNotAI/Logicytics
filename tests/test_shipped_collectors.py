ecfab0f57e03080083af8f3b493bd922425e548a
DefinetlyNotAI <Shahm_Najeeb@outlook.com>
2026-08-16 20:31:21 +0300

    feat: add group policy collector

--- tests/test_shipped_collectors.py
+++ tests/test_shipped_collectors.py
@@ -26,6 +26,7 @@ class ShippedCollectorTests(unittest.TestCase):
                 "core.storage.logical_drives",
                 "core.system.system_info",
                 "core.system.installed_drivers",
+                "core.system.group_policy",
                 "core.usb.usb_storage_inventory",
                 "core.hardware.windows_features",
             },
